8a2a3ef6ecd9e213b667c9d27a012cc2a22d80f0
cedric816 <cedricnoisette9@gmail.com>
2021-08-24 14:35:25 +0200

    css

--- webpack.config.js
+++ webpack.config.js
@@ -59,6 +59,9 @@ Encore
     // enables Sass/SCSS support
     .enableSassLoader()
 
+    //enable VueJS
+    .enableVueLoader()
+
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
 
